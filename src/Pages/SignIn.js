573be59f434f22bb55ad7403d1a7c4a5edda7d01
shaibal-tiller <shaibal.tiller@gmail.com>
2024-01-30 17:43:58 +0600

    bg animation

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -7,20 +7,26 @@ import login_animation from '../Assets/home.gif'
 const SignIn = () => {
 
     const [loading, setLoading] = useState(false)
-    const signInWithGoogle = async () => {
+    const [user, setUser] = useState(null)
+    const signInWithGoogle = async (e) => {
 
         const provider = new GoogleAuthProvider();
 
         try {
             signInWithPopup(auth, provider).then(res => {
-
+                setUser(res);
+                setLoading(false)
             })
                 .catch(e => {
+                    setLoading(false)
                     console.log(e);
                 })
         } catch (error) {
-            console.error(error.message);
+            setLoading(false)
+            console.log(error.message);
         }
+
+
     };
 
     return (
@@ -29,9 +35,13 @@ const SignIn = () => {
             <img src={login_animation} className='h-1/2 mx-auto' />
 
 
-            <button className='py-1 hover:border-expense-light border-dark-1 hover:bg-dark-1 bg-opacity-30 hover:scale-105 active:scale-95 z-10 mx-auto flex  items-center rounded-md border-2 bg-gray-300  font-semibold font-mono mt-6'
-                onClick={signInWithGoogle}>
-                <span className='w-2/5 '><img className=' h-10' src={google} /></span><p className='w-[150%] '>Sign in with Google</p></button>
+            {!loading ? <button className='py-1 hover:border-expense-light border-dark-1 hover:bg-dark-1 bg-opacity-30 hover:scale-105 active:scale-95 z-10 mx-auto flex  items-center rounded-md border-2 bg-gray-300  font-semibold font-mono mt-6'
+                onClick={(e) => { setLoading(true); signInWithGoogle() }}>
+                <span className='w-2/5 '><img className=' h-10' src={google} /></span><p className='w-[150%] '>Sign in with Google</p></button> :
+                <div class="relative flex justify-center items-center mt-12 text-center">
+                    <p className='absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 font-semibold text-expense-light text-lg'>Loading</p>
+                    <div class="animate-spin rounded-full h-24 w-24 border-t-2 border-b-2 border-dark-1">  </div>
+                </div>}
         </div>
     );
 };
