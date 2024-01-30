4642e5b1c3533d6e79fa3528cc4dd4da749f0cf5
shaibal-tiller <shaibal.tiller@gmail.com>
2024-01-30 17:32:29 +0600

    bg animation

--- src/Pages/SignIn.js
+++ src/Pages/SignIn.js
@@ -1,10 +1,12 @@
 // SignIn.js
-import React from 'react';
+import React, { useState } from 'react';
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { auth } from '../Components/firebaseConfig'; // Import initialized Firebase app
-import logo from '../Assets/mman_logo.png'
 import google from '../Assets/google.png'
+import login_animation from '../Assets/home.gif'
 const SignIn = () => {
+
+    const [loading, setLoading] = useState(false)
     const signInWithGoogle = async () => {
 
         const provider = new GoogleAuthProvider();
@@ -22,12 +24,14 @@ const SignIn = () => {
     };
 
     return (
-        <div className='text-center'>
-           
-          <img/>
-            <button className='mx-auto flex  items-center rounded-md border-2 bg-gray-300  font-semibold font-mono mt-6' 
-            onClick={signInWithGoogle}>
-                <span className='w-2/5 '><img className=' h-10' src={google}/></span><p className='w-[150%] '>Sign in with Google</p></button>
+        <div className='text-center w-3/4 md:w-1/2 bg-white h-[40vh] bg-opacity-90 mx-auto  rounded-md mt-8'>
+
+            <img src={login_animation} className='h-1/2 mx-auto' />
+
+
+            <button className='py-1 hover:border-expense-light border-dark-1 hover:bg-dark-1 bg-opacity-30 hover:scale-105 active:scale-95 z-10 mx-auto flex  items-center rounded-md border-2 bg-gray-300  font-semibold font-mono mt-6'
+                onClick={signInWithGoogle}>
+                <span className='w-2/5 '><img className=' h-10' src={google} /></span><p className='w-[150%] '>Sign in with Google</p></button>
         </div>
     );
 };
